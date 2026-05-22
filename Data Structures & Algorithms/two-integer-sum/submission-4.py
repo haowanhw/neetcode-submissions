07dfb737b9968ad65963994db0dbc4c2ad9bdb4f
Hao Wan <hw.haowan@gmail.com>
2026-05-21 23:34:48 -0400

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            residual = target - nums[i]
+            for j in range(len(nums)):
+                if nums[j] == residual and i != j:
+                    return [i, j]
